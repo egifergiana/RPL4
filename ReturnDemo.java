fb73fa13c31595bfcebb4c23cb93745ab98e16f8
egifergiana <egi_fergiana@yahoo>
2014-09-22 15:01:05 +0700

    Kelompok Doa Ibu

---
+++ ReturnDemo.java
@@ -0,0 +1,9 @@
+class ReturnDemo {
+	public static void main (String [] args){
+	boolean t= true;
+	System.out.println("Sebelum return ");
+	if(t)
+	return;
+	System.out.println("Peryataan ini tidak akan di eksekusi");
+	}
+	}
